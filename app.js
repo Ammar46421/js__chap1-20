09e380c41a05a6d69cb9aef70e6c919e097b208f
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-08-26 17:18:38 +0500

    update

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-var passcode;
+// var passcode;
 
-while (true) {
-    passcode = prompt("enter the passcode to start website ")
-    if (passcode === "1234" ) {
-        break;
-    }else if(passcode === "2468"){
-        break;
-    }
-    alert("Wrong Passcode,Try Again")
+// while (true) {
+//     passcode = prompt("enter the passcode to start website ")
+//     if (passcode === "1234" ) {
+//         break;
+//     }else if(passcode === "2468"){
+//         break;
+//     }
+//     alert("Wrong Passcode,Try Again")
+// }
+// alert("Correct Passcode")
+
+// // chap 10 & 11
+// var x = prompt("where is karachi?");
+// if (x === "Pakistan") {
+//     alert("Correct!")
+// };
+
+// if (x != "Pakistan") {
+//     alert("Incorrect!")
+// };
+
+// //chap 12
+
+var yourTicketNumber = prompt("Input Your Ticket Number.");
+
+if (yourTicketNumber == 4) {
+    alert("you won ");
 }
-alert("Correct Passcode")
+
+else {
+    alert("Better luck next time.");
+}
+
+
+// // chap 13
+// // if and else statement
+// var age = prompt("Enter Your Age.");
+
+// if (age <= 17) {
+//     alert("Pay Rs10.")
+// };
+
+// if (age >= 18 && age <= 59) {
+//     alert("Pay Rs20.")
+// }
+// else {
+//     alert("Free Entry.")
+// };
 
